aa391abeff82aa40c9c3850cd90f7ab55f1a2f6b
TheEyeOfAr3s <eherzig@protonmail.com>
2022-08-20 20:17:58 +0200

    v1.0.0

--- src/mod.ts
+++ src/mod.ts
@@ -1,21 +1,21 @@
 import { DependencyContainer } from "tsyringe";
-import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
-import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
-import { LogBackgroundColor } from "@spt-aki/models/spt/logging/LogBackgroundColor";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-class Mod implements IPreAkiLoadMod
+class Mod implements IPostDBLoadMod
 {
-    // Code added here will load BEFORE the server has started loading
-    preAkiLoad(container: DependencyContainer): void 
-    {
-        // get the logger from the server container
-        const logger = container.resolve<ILogger>("WinstonLogger");
-        
-        logger.info("I am logging info!");
-        logger.warning("I am logging a warning!");
-        logger.error("I am logging an error!");
-        logger.logWithColor("I am logging with color!", LogTextColor.YELLOW, LogBackgroundColor.RED);
+    private cfg = require("../config.json");
+
+    postDBLoad(container: DependencyContainer): void {
+        if (this.cfg.enabled)
+        {
+            const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+            const tables = databaseServer.getTables();
+            const rk2 = tables.templates.items["5c1bc5af2e221602b412949b"];
+
+            rk2._props.Ergonomics = this.cfg.ergo;
+            rk2._props.Recoil = this.cfg.recoil;
+        }
     }
 }
 
