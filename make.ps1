aa78d270a7fa9578f04bea18287948a99579aae4
TheEyeOfAr3s <eherzig@protonmail.com>
2022-08-20 19:41:38 +0200

    first commit

---
+++ make.ps1
@@ -0,0 +1,12 @@
+npm run clean:environment | out-null
+npm run build:unzipped | out-null
+if (Test-Path -Path C:\Games\ETM\user\mods\oprk2\) {
+    Remove-Item -Recurse -Force C:\Games\ETM\user\mods\oprk2\ | out-null
+}
+mkdir C:\Games\ETM\user\mods\oprk2\ | out-null
+Copy-Item -Recurse -Path .\dist\* -Destination C:\Games\ETM\user\mods\oprk2\ | out-null
+Remove-Item C:\Games\ETM\user\mods\WhitePhosphor\make.ps1 | out-null
+Set-Location C:\Games\ETM\ | out-null
+Start-Process powershell {./Aki.Server.exe}
+Start-Process powershell {./Aki.Launcher.exe}
+Set-Location C:\Users\TheEyeOfAr3s\Documents\coding\SPTarkov\OP_RK2\ | out-null
